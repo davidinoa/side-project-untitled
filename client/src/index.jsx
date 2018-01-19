8e559070c0ded144d8b18eff644c8063920a55dc
David Inoa <davidinoa@gmail.com>
2018-01-19 00:42:58 -0500

    Integrate React Router to the app

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,35 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import List from './components/List.jsx';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// App components
+import Home from './components/Home.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      items: []
-    };
-  }
-
-  componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
-        this.setState({
-          items: data
-        });
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
   }
 
   render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
-    </div>);
+    return (
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={Home} />
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
