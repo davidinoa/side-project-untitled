eafb06e847bedb7c84a0176246f2ffc4b21bae28
David Inoa <davidinoa@gmail.com>
2018-01-21 18:22:12 -0500

    Inject data into the app

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,23 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+// data
+const data = require('./data/houseMembersRI.js');
+
 // App components
 import Home from './components/Home.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    
+    this.state = {
+      reps: this.props.reps
+    };
   }
 
   render () {
     return (
       <BrowserRouter>
         <div>
-          <Route exact path="/" component={Home} />
+          <Route 
+            exact path="/" 
+            render={ () => <Home reps={this.state.reps} /> } />
         </div>
       </BrowserRouter>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App reps={data}/>, document.getElementById('app'));
